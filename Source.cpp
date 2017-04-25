fabdc46094b75c3883c85c22ff4dd7914d8e67b3
Sebastian Weber <seweber@posteo.de>
2017-04-25 11:08:10 +0200

    Add TODOs, prepare the usage of Eigen

--- Source.cpp
+++ Source.cpp
@@ -1,8 +1,17 @@
 # include <iostream>
+//#include <Eigen/Dense>
+
 using namespace std;
 int main()
 {
-	cout << "This is test file";
+    cout << "This is test file";
 
-	return 0;
+    /*Eigen::MatrixXd m(2,2);
+    m(0,0) = 3;
+    m(1,0) = 2.5;
+    m(0,1) = -1;
+    m(1,1) = m(1,0) + m(0,1);
+    std::cout << m << std::endl;*/
+
+    return 0;
 }
