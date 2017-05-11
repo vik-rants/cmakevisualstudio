02a0f35444dcdfa1f1bc477e016495099ef09cca
biswasvikrant <vikrant.nitd@gmail.com>
2017-05-11 12:39:31 +0200

    Fixes

--- ci/main.cpp
+++ ci/main.cpp
@@ -4,7 +4,7 @@
 using namespace std;
 int main()
 {
-    cout << "This is test file";
+    cout << "This is test file for Eigen ";
 
     Eigen::MatrixXd m(2,2);
     m(0,0) = 3;
@@ -12,6 +12,6 @@ int main()
     m(0,1) = -1;
     m(1,1) = m(1,0) + m(0,1);
     std::cout << m << std::endl;
-
+	system("PAUSE");
     return 0;
 }
