7e078fe54b2b14bc6801b3bef155bf002e4075fd
biswasvikrant <vikrant.nitd@gmail.com>
2017-05-12 13:31:05 +0200

    sqladd

--- cmake/FindSqlite3.cmake
+++ cmake/FindSqlite3.cmake
@@ -17,17 +17,35 @@
 # SQLITE3_LIBRARY       - the sqlite3 library
 # SQLITE3_INCLUDE_DIR   - path including sqlite3.h
 # SQLITE3_BINARY        - sqlite3 executable
+#
+# SQLITE3_ROOT - Set as Env variable
+#
 
 # Include these modules to handle the QUIETLY and REQUIRED arguments.
 include (FindPackageHandleStandardArgs)
 
-find_path( SQLITE3_INCLUDE_DIR NAMES sqlite3.h )
+find_path( SQLITE3_INCLUDE_DIR NAMES sqlite3.h
+	HINTS
+    ENV SQLITE3_ROOT 
+	)
 
 find_library( SQLITE3_LIBRARY NAMES sqlite3 )
 
-find_program( SQLITE3_BINARY NAMES sqlite3 )
+#find_program( SQLITE3_BINARY NAMES sqlite3 )
 
 # Set the FOUND variable to TRUE if all listed variables are set.
-find_package_handle_standard_args( SQLITE3 DEFAULT_MSG SQLITE3_LIBRARY SQLITE3_INCLUDE_DIR SQLITE3_BINARY )
+
+INCLUDE(FindPackageHandleStandardArgs)
+
+find_package_handle_standard_args( SQLITE3 DEFAULT_MSG SQLITE3_LIBRARY SQLITE3_INCLUDE_DIR)
+
+# Copy the results to the output variables.
+IF(SQLITE3_FOUND)
+	SET(SQLITE3_LIBRARIES ${SQLITE3_LIBRARY})
+	SET(SQLITE3_INCLUDE_DIRS ${SQLITE3_INCLUDE_DIR})
+ELSE(SQLITE3_FOUND)
+	SET(SQLITE3_LIBRARIES)
+	SET(SQLITE3_INCLUDE_DIRS)
+ENDIF(SQLITE3_FOUND)
 
 mark_as_advanced( SQLITE3_LIBRARY SQLITE3_INCLUDE_DIR SQLITE3_BINARY )
