6b0a4d8a8b8beac5178b0f43045c55289c302208
biswasvikrant <vikrant.nitd@gmail.com>
2017-05-25 11:06:59 +0200

    Py

--- cmake/FindPyUIC.cmake
+++ cmake/FindPyUIC.cmake
@@ -19,7 +19,7 @@
 # Include these modules to handle the QUIETLY and REQUIRED arguments.
 include (FindPackageHandleStandardArgs)
 
-find_program( PYUIC_BINARY NAMES py3uic5 pyuic
+find_program( PYUIC_BINARY NAMES py3uic5 pyuic5
 				HINTS
 				ENV PyUIC_ROOT
 				PATHS
