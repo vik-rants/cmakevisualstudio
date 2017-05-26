fae3cbb0b763bcef47e1cdeed9e7a27037a4f37d
biswasvikrant <vikrant.nitd@gmail.com>
2017-05-26 15:54:10 +0200

    P

--- cmake/FindSqlite3.cmake
+++ cmake/FindSqlite3.cmake
@@ -24,11 +24,20 @@ include (FindPackageHandleStandardArgs)
 find_path( SQLITE3_INCLUDE_DIR NAMES sqlite3.h
 			HINTS
 			ENV SQLITE3_ROOT 
+			PATHS
+			${SQLITE3_ROOT}/sqlite-amalgamation-3190200
+			PATH_SUFFIXES include
 			)
 
-find_library( SQLITE3_LIBRARY NAMES sqlite3.dll )
+find_library( SQLITE3_LIBRARY NAMES sqlite3.dll 
+			 HINTS
+			 ENV SQLITE3_ROOT 
+			)
 
-find_program( SQLITE3_BINARY NAMES sqlite3.exe )
+find_program( SQLITE3_BINARY NAMES sqlite3.exe
+			 HINTS
+			 ENV SQLITE3_ROOT 
+			)
 
 # Set the FOUND variable to TRUE if all listed variables are set.
 find_package_handle_standard_args( SQLITE3 DEFAULT_MSG SQLITE3_LIBRARY SQLITE3_INCLUDE_DIR SQLITE3_BINARY )
